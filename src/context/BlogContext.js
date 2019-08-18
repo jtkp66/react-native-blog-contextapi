7514e61a87bc893704e99f12a0f1193ce5abdafe
Jamie Pham <jtkp66@gmail.com>
2019-08-18 16:20:03 -0700

    adding edit form, initializing state from Context, and added edit icon to showScreen

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -34,5 +34,5 @@ const deleteBlogPost = dispatch => {
 export const { Context, Provider } = createDataContext(
   blogReducer,
   { addBlogPost, deleteBlogPost },
-  []
+  [{ title: "Test Post", content: "Test Content", id: 1 }]
 );
