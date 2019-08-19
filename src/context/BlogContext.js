0ef53dfd5574c186d041232f4851b750ce7848b8
Jamie Pham <jtkp66@gmail.com>
2019-08-18 20:53:06 -0700

    adding edit blog to reducer, and navigation after editing a post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,6 +2,10 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "edit_blogpost":
+      return state.map(blogPost => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case "delete_blogpost":
       return state.filter(blogPost => blogPost.id !== action.payload);
     case "add_blogpost":
@@ -21,7 +25,9 @@ const blogReducer = (state, action) => {
 const addBlogPost = dispatch => {
   return (title, content, callBack) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
-    callBack();
+    if (callBack) {
+      callBack();
+    }
   };
 };
 
@@ -32,8 +38,11 @@ const deleteBlogPost = dispatch => {
 };
 
 const editBlogPost = dispatch => {
-  return (id, title, content) => {
-    dispatch({ type: "edit_blogpost", payload: id, title, content });
+  return (id, title, content, callBack) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if (callBack) {
+      callBack();
+    }
   };
 };
 
